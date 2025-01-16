d7d07a4094a010574aee1b5d745d46f273c98ea5
MiloszMichalak <miloszmichalak150@gmail.com>
2025-01-16 22:33:36 +0100

    Dodano kod dodajacy dane do sql

--- 3 klasa/php/Laczenie z baza/index.php
+++ 3 klasa/php/Laczenie z baza/index.php
@@ -1,23 +1,65 @@
-<?php
-$con = new mysqli("localhost", "root", "", "taryfikator");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie PHP</title>
+</head>
+<body>
+<h1>Tworzenie bazy danych</h1>
+<form method="post" action="index.php">
+    <label for="dbName">Nazwa bazy danych</label>
+    <input type="text" id="dbName" name="dbName" required> &nbsp;
+    <label for="tableName">Nazwa tabeli</label>
+    <input type="text" id="tableName" name="tableName1" required><br><br>
 
-$query = "SELECT * FROM kierowcy_txt";
 
-$result = $con->query($query);
-echo "<table>";
-echo "<tr>
-        <td>IdOsoby</td>
-        <td>Imie</td>
-        <td>Nazwisko</td>
-        <td>NrRejestracyjny</td>
-      </tr>";
+    <label for="fieldName1">Pole w tabeli 1</label>
+    <input type="text" id="fieldName1" name="fieldName1" required><br><br>
+    <label for="fieldName2">Pole w tabeli 2</label>
+    <input type="text" id="fieldName2" name="fieldName2" required><br><br>
 
-while($row = $result->fetch_assoc()){
-    echo "<tr>";
-    echo "<td>".$row['IdOsoby']."</td>";
-    echo "<td>".$row['Imie']."</td>";
-    echo "<td>".$row['Nazwisko']."</td>";
-    echo "<td>".$row['NrRejestracyjny']."</td>";
-    echo "</tr>";
-}
-echo "</table>";
+    <button type="submit">Zatwierdz</button>
+    <br>
+    
+    <?php
+        $server = "localhost";
+        $user = "root";
+        $password = "";
+        
+        $con = new mysqli($server, $user, $password);
+        
+        if (isset($_POST["dbName"])) {
+            if ($con->connect_error) {
+                die("Polaczenie nieudane: ".$con->connect_error);
+            }
+            
+            $db = $_POST["dbName"];
+            
+            $query = "CREATE DATABASE IF NOT EXISTS ".$db;
+            if ($con->query($query) === TRUE) {
+                echo "Baza stworzono prawidłowa"."<br>";
+            } else {
+                echo "Nie udalo sie stworzyc bazy danych: ".$con->error;
+            }
+            
+            $con->query("USE ".$db);
+            
+            $sql = "CREATE TABLE 
+                {$_POST['tableName1']}(id INT AUTO_INCREMENT PRIMARY KEY, 
+                {$_POST['fieldName1']} INT NOT NULL, 
+                {$_POST['fieldName2']} INT NOT NULL)";
+            
+            if ($con->query($sql) === TRUE) {
+                echo "Tabela z polami stworzona pomyslnie";
+            } else {
+                echo "Blad przy tworzeniu tabel z polami: ".$con->error;
+            }
+            
+            $con->close();
+        }
+        ?>
+</form>
+</body>
+</html>
+
+
